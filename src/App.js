dabd67225778f096fd515507078792f5ed6f41a0
unknown <rmtestard@gmail.com>
2019-07-05 09:22:55 +0200

    project setup Header Contacts Contact component creation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Contacts from './components/Contacts';
+import Header from './components/Header';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div>
+      <Header branding="Contact Manager" />
+      <div className="container">
+        <Contacts />
+      </div>
+    </div>
+  );
 }
 
 export default App;
