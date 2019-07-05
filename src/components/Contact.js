c3597e010163ddeb58aa6b0d1df4fb813e830d87
rtestard <rtestard@opensense.fr>
2019-07-05 18:00:17 +0200

    showing data on click

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,40 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Contact extends Component {
   static propTypes = {
     contact: PropTypes.object.isRequired
   };
 
+  constructor() {
+    super();
+    this.state = {
+      showContactInfo: true
+    };
+    this.onShowClick = this.onShowClick.bind(this);
+  }
+
+  onShowClick() {
+    this.setState({
+      showContactInfo: !this.state.showContactInfo
+    });
+  }
+
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">{email}</li>
-          <li className="list-group-item">{phone}</li>
-        </ul>
+        <h4>
+          {name}
+          <i onClick={this.onShowClick} className="fas fa-sort-down" />
+        </h4>
+        {showContactInfo && (
+          <ul className="list-group">
+            <li className="list-group-item">{email}</li>
+            <li className="list-group-item">{phone}</li>
+          </ul>
+        )}
       </div>
     );
   }
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import Contact from './Contact';
+import React, { Component, Fragment } from "react";
+import Contact from "./Contact";
 
 class Contacts extends Component {
   state = {
     contacts: [
       {
         id: 1,
-        name: 'Romain Testard',
-        email: 'r@gmail.com',
-        phone: '666-666-6666'
+        name: "Romain Testard",
+        email: "r@gmail.com",
+        phone: "666-666-6666"
       },
       {
         id: 2,
-        name: 'Maiha Nguyen',
-        email: 'mn@gmail.com',
-        phone: '111-111-1111'
+        name: "Maiha Nguyen",
+        email: "mn@gmail.com",
+        phone: "111-111-1111"
       }
     ]
   };
@@ -23,11 +23,11 @@ class Contacts extends Component {
     const { contacts } = this.state;
 
     return (
-      <div>
+      <Fragment>
         {contacts.map(contact => (
           <Contact key={contact.id} contact={contact} />
         ))}
-      </div>
+      </Fragment>
     );
   }
 }
