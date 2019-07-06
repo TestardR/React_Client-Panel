ef26f6ab31f31012db4c5d6e9ad77d47ccdf40d3
unknown <rmtestard@gmail.com>
2019-07-06 13:26:25 +0200

    form creation

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -7,35 +7,47 @@ class AddContact extends Component {
     phone: ''
   };
   render() {
+    const { name, email, phone } = this.state;
     return (
       <div className="card mb-3">
         <h1 className="card-header">Add Contact</h1>
         <div className="card-body">
-          <form action="form-group">
-            <label htmlFor="name">Name</label>
+          <form>
+            <div action="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="form-control form-control-lg"
+                placeholder="Enter Name..."
+                value={name}
+              />
+            </div>
+            <div action="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="form-control form-control-lg"
+                placeholder="Enter Email..."
+                value={email}
+              />
+            </div>
+            <div action="form-group">
+              <label htmlFor="phone">Phone</label>
+              <input
+                type="text"
+                name="phone"
+                className="form-control form-control-lg"
+                placeholder="Enter Phone..."
+                value={phone}
+              />
+            </div>
             <input
-              type="text"
-              name="name"
-              className="form-control form-control-lg"
-              placeholder="Enter Name..."
-            />
-          </form>
-          <form action="form-group">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              className="form-control form-control-lg"
-              placeholder="Enter Email..."
-            />
-          </form>
-          <form action="form-group">
-            <label htmlFor="phone">phone</label>
-            <input
-              type="text"
-              name="Phone"
-              className="form-control form-control-lg"
-              placeholder="Enter Phone..."
+              type="submit"
+              value="Add Contact"
+              className="btn btn-light btn-block"
+              style={{ marginTop: '1rem' }}
             />
           </form>
         </div>
